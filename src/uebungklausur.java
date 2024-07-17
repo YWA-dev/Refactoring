99ab817457d5a5c7f1e5901eb660c85816b5cb2b
CodingWagner <yannik.wagner.1@gmx.de>
2024-07-17 11:14:14 +0200

    Refactoring 17.7.24

--- src/uebungklausur.java
+++
@@ -1,9 +0,0 @@
-public class uebungklausur  {
-    public static void main(String[] args) {
-
-        Veranstaltung veranstaltung = new Veranstaltung("Nature one");
-        Saalplan saalplan1 = new Saalplan("Müllem",5, "09-08-2000",veranstaltung);
-        saalplan1.ausfgabe();
-        saalplan1.
-    }
-}
